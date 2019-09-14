bcbb71c53d49fbb66c1c33ecc93d3deaf8eb49b7
ahrasel <ahrasel95@gmail.com>
2019-09-15 01:05:42 +0600

    home page low inventory transection n expired soon done

--- PosSustemUIU/Models/BLL/TransectionManager.cs
+++ PosSustemUIU/Models/BLL/TransectionManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
@@ -22,6 +23,20 @@ namespace PosSustemUIU.Models.BLL
 
         public async System.Threading.Tasks.Task<List<Transection>> GetLowInventoryAsync(){
             var transections = _context.Transections.Where(t => t.TransectionTypeId == "bda54eb3-c4ea-4a52-a488-9fdaf2bb6e8d" && t.RemainingQuantity < 10).Include(t => t.Product).Include(t => t.TransectionType);
+            
+            return await transections.ToListAsync();
+        }
+
+        public async System.Threading.Tasks.Task<List<Transection>> GetProductExpiredSoonAsync(){
+            var date = DateTime.Now.AddDays(7);
+            var transections = _context.Transections.Where(t => t.TransectionTypeId == "bda54eb3-c4ea-4a52-a488-9fdaf2bb6e8d" && t.ExpireDate < date).Include(t => t.Product).Include(t => t.TransectionType);
+            
+            return await transections.ToListAsync();
+        }
+
+        public async System.Threading.Tasks.Task<List<Transection>> GetTodaysTransectionAsync(){
+            var date = DateTime.Today;
+            var transections = _context.Transections.Where(t => t.ExpireDate == date).Include(t => t.Product).Include(t => t.TransectionType);
             
             return await transections.ToListAsync();
         }
